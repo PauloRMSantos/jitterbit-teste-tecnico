f7bd1f5ef0d01f66a0f088caba14964042d85b76
PauloRoberto <paulo.rosantos@rede.ulbra.br>
2026-03-07 18:51:06 -0300

    Criação do arquivos de rotas

---
+++ src/router.js
@@ -0,0 +1,33 @@
+function router(req, res) {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/order/list') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'listar pedidos' }));
+
+  } else if (method === 'GET' && url.startsWith('/order/')) {
+    const orderId = url.split('/')[2]; // extrai o ID da URL
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: `buscar pedido ${orderId}` }));
+
+  } else if (method === 'POST' && url === '/order') {
+    res.writeHead(201, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'criar pedido' }));
+
+  } else if (method === 'PUT' && url.startsWith('/order/')) {
+    const orderId = url.split('/')[2];
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: `atualizar pedido ${orderId}` }));
+
+  } else if (method === 'DELETE' && url.startsWith('/order/')) {
+    const orderId = url.split('/')[2];
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: `deletar pedido ${orderId}` }));
+
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Rota não encontrada.' }));
+  }
+}
+
+module.exports = router;
