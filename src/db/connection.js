78d8d5595b36f4585fdc42925e704c16ef647e3c
PauloRoberto <paulo.rosantos@rede.ulbra.br>
2026-03-07 19:19:58 -0300

    Adicionada conexão com banco via docker

---
+++ src/db/connection.js
@@ -0,0 +1,13 @@
+// Estou usando o Pool ao invés do Client para gerenciar conexões de forma mais eficiente
+const { Pool } = require('pg') 
+
+// Como é apenas um teste técnico, até não haveria necessidade, mas preferi usar o dotenv para simular um case real
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+module.exports = pool;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+require ('dotenv').config();
+
 const http = require('http');
+const pool = require('./db/connection');
 const router = require('./router');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  router(req, res);
-});
+async function startServer() {
+    try {
+        await pool.query('SELECT 1')
+        console.log('Conexão ok')
 
-server.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-});
+        const server = http.createServer((req, res) => {
+            router(req, res);
+        })
+
+        server.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        })
+    } catch (err) {
+        console.error('Erro de conexão:', err);
+        process.exit(1);
+    }
+}
+
+startServer();
